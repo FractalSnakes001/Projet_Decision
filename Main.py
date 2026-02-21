4ddba935a02dea9b898c2a4bb708b1c61c5cff81
Paolo <p.decerteau@gmail.com>
2026-02-21 17:28:41 +0100

    Set up pour les gens sur venv

--- Main.py
+++ Main.py
@@ -1,5 +1,8 @@
 #(paolo) j'ai mit un fichier YML qui permet normalement de creer l'environnement que j'utilise
 # comme cela on a tous les memes versions de biblio...etc 
+# CONDA: conda env create -f env_python_CONDA.yml.  (puis apres vous activez )
+# venv/pip: 1) creer un nouveau environnement, activez le puis : pip install -r env_python_PIP.txt
 
 
 print("ok?")
+print("test")
